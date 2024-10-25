e36391f539f8cbcb2b52e77650acd50ac4c1f7a7
Levy A. <levyddsa@gmail.com>
2024-10-25 15:57:13 -0300

    feat: encryption support

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ set -xe
 
 cd libsql-c
 
-cargo zigbuild --target universal2-apple-darwin --release
-cargo zigbuild --target aarch64-unknown-linux-gnu --release
-cargo zigbuild --target x86_64-unknown-linux-gnu --release
+./build.sh
 
 rm -rf ../lib
 
