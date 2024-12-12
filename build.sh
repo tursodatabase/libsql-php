631917384fa0a9197d87c8a2624c036755766e98
Levy A. <levyddsa@gmail.com>
2024-12-12 11:10:44 -0300

    fix libs

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ rm -rf ../lib
 mkdir -p \
   ../lib/aarch64-unknown-linux-gnu \
   ../lib/x86_64-unknown-linux-gnu \
-  ../lib/universal2-apple-darwin \
+  ../lib/aarch64-apple-darwin \
 
 cp ./libsql.h ../lib/libsql.h
-cp ./target/x86_64-unknown-linux-gnu/release/liblibsql.so ../lib/x86_64-unknown-linux-gnu/
 cp ./target/aarch64-unknown-linux-gnu/release/liblibsql.so ../lib/aarch64-unknown-linux-gnu/
-cp ./target/universal2-apple-darwin/release/liblibsql.dylib ../lib/universal2-apple-darwin/
+cp ./target/x86_64-unknown-linux-gnu/release/liblibsql.so ../lib/x86_64-unknown-linux-gnu/
+cp ./target/aarch64-apple-darwin/release/liblibsql.dylib ../lib/aarch64-apple-darwin/
--- src/Database.php
+++ src/Database.php
@@ -24,8 +24,7 @@ function getFFI(): ?FFI
         $ffi = FFI::cdef(
             file_get_contents(__DIR__ . '/../lib/libsql.h'),
             __DIR__ . match ([$os, $arch]) {
-                ["Darwin", "arm64"] => '/../lib/universal2-apple-darwin/liblibsql.dylib',
-                ["Darwin", "x86_64"] => '/../lib/universal2-apple-darwin/liblibsql.dylib',
+                ["Darwin", "arm64"] => '/../lib/aarch64-apple-darwin/liblibsql.dylib',
                 ["Linux", "x86_64"] => '/../lib/x86_64-unknown-linux-gnu/liblibsql.so',
                 ["Linux", "arm64"] => '/../lib/aarch64-unknown-linux-gnu/liblibsql.so',
                 default => die("Unsupported OS $os $arch"),
